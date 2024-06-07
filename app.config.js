367e7642fa7087d4c2518a92b8113c2ae267891e
Roy Shnap <royshnap@gmail.com>
2024-06-07 19:46:24 +0300

    added apk

--- app.config.js
+++ app.config.js
@@ -1,5 +1,15 @@
-module.exports = {
+module.exports = ({ config }) => ({
+    ...config,
+    slug: "meetinclick",  // Ensure this matches the EAS project slug
+    android: {
+      package: "com.royshnap.meetinclick"  // Replace with your unique package identifier
+    },
     extra: {
-        clerkPublishableKey: process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY
+      ...config.extra,
+      eas: {
+        projectId: "c2efac3c-c482-4574-9987-5a8f7d523ca3"
+      },
+      clerkPublishableKey: process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY
     }
-}
+  });
+  
