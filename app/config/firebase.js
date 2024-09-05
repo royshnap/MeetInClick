016391ca9257f1710ba79ad8a6b290bac0d201b6
ofir22peled <ofir22peled@gmail.com>
2024-09-05 13:24:41 +0300

    added login with google

--- app/config/firebase.js
+++ app/config/firebase.js
@@ -1,36 +1,73 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
+// // Import the functions you need from the SDKs you need
+// import { initializeApp } from "firebase/app";
 
-import { getAuth, initializeAuth ,getReactNativePersistence} from "firebase/auth";
+// import { getAuth, initializeAuth ,getReactNativePersistence} from "firebase/auth";
 
-import ReactNativeAsyncStorage from '@react-native-async-storage/async-storage';
-import { getDatabase } from "firebase/database";
-import { getStorage } from "firebase/storage";
+// import ReactNativeAsyncStorage from '@react-native-async-storage/async-storage';
+// import { getDatabase } from "firebase/database";
+// import { getStorage } from "firebase/storage";
 
-import 'firebase/storage';
-// TODO: Add SDKs for Firebase products that you want to usea
-// https://firebase.google.com/docs/web/setup#available-libraries
+// import 'firebase/storage';
 
-// Your web app's Firebase configuration
+// // TODO: Add SDKs for Firebase products that you want to usea
+// // https://firebase.google.com/docs/web/setup#available-libraries
+
+// // Your web app's Firebase configuration
+// const firebaseConfig = {
+//   apiKey: "AIzaSyAUiO1xapzn79QNxQEy6k23pvmX2lcXJ0o",
+//   authDomain: "meetinclick.firebaseapp.com",
+//   databaseURL: "https://meetinclick-default-rtdb.europe-west1.firebasedatabase.app",
+//   projectId: "meetinclick",
+//   storageBucket: "meetinclick.appspot.com",
+//   messagingSenderId: "1010390116818",
+//   appId: "1:1010390116818:web:f8e38c69140e4470e0400b"
+// };
+
+// // Initialize Firebase
+// const app = initializeApp(firebaseConfig);
+
+// const Firebase = {
+//     Auth: initializeAuth(app, {
+//         persistence: getReactNativePersistence(ReactNativeAsyncStorage)
+//       }),
+//     Database: getDatabase(app),
+//     Storage: getStorage(app)
+
+// }
+// export default Firebase
+
+import { initializeApp } from 'firebase/app';
+import {
+  getAuth,
+  GoogleAuthProvider,
+  initializeAuth,
+  getReactNativePersistence,
+} from 'firebase/auth';
+import { getDatabase } from 'firebase/database';
+import { getStorage } from 'firebase/storage';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
 const firebaseConfig = {
-  apiKey: "AIzaSyAUiO1xapzn79QNxQEy6k23pvmX2lcXJ0o",
-  authDomain: "meetinclick.firebaseapp.com",
-  databaseURL: "https://meetinclick-default-rtdb.europe-west1.firebasedatabase.app",
-  projectId: "meetinclick",
-  storageBucket: "meetinclick.appspot.com",
-  messagingSenderId: "1010390116818",
-  appId: "1:1010390116818:web:f8e38c69140e4470e0400b"
+  apiKey: 'AIzaSyAUiO1xapzn79QNxQEy6k23pvmX2lcXJ0o',
+  authDomain: 'meetinclick.firebaseapp.com',
+  databaseURL:
+    'https://meetinclick-default-rtdb.europe-west1.firebasedatabase.app',
+  projectId: 'meetinclick',
+  storageBucket: 'meetinclick.appspot.com',
+  messagingSenderId: '1010390116818',
+  appId: '1:1010390116818:web:f8e38c69140e4470e0400b',
 };
 
-// Initialize Firebase
+// Initialize Firebase App
 const app = initializeApp(firebaseConfig);
 
-const Firebase = {
-    Auth: initializeAuth(app, {
-        persistence: getReactNativePersistence(ReactNativeAsyncStorage)
-      }),
-    Database: getDatabase(app),
-    Storage: getStorage(app)
+// Initialize Firebase Auth with persistence
+const auth = initializeAuth(app, {
+  persistence: getReactNativePersistence(AsyncStorage),
+});
 
-}
-export default Firebase
+// Initialize other Firebase services
+const database = getDatabase(app);
+const storage = getStorage(app);
+
+export { auth, GoogleAuthProvider, database, storage };
--- app/context/GoogleSignIn.js
+++ app/context/GoogleSignIn.js
@@ -1,23 +1,29 @@
 import React from 'react';
 import { TouchableOpacity, Text, StyleSheet, Alert } from 'react-native';
 import * as Google from 'expo-auth-session/providers/google';
-import firebase from '../config/firebase';
-import 'firebase/auth';
-import 'firebase/firestore';
+import { auth, GoogleAuthProvider } from '../config/firebase';
 
 const GoogleSignIn = () => {
   const [request, response, promptAsync] = Google.useAuthRequest({
     expoClientId: '63eb3457-b53f-4a3b-8c0f-04d8e653773f',
-    iosClientId: '793761719542-obfbf3g0u2e95r4sbj88l4j6erg20lfs.apps.googleusercontent.com',
-    androidClientId: '793761719542-4ibctgdab9mb5t69va7q7uohaolu9bvl.apps.googleusercontent.com',
-    webClientId: '793761719542-5dfr6olefnlj5h4hur9bl1qmahv4rh50.apps.googleusercontent.com',
+    //iosClientId: '793761719542-obfbf3g0u2e95r4sbj88l4j6erg20lfs.apps.googleusercontent.com',
+    iosClientId:
+      '595812871102-orhd01f1kuktrbr7ea053sphln6ordh7.apps.googleusercontent.com',
+    //androidClientId: '793761719542-4ibctgdab9mb5t69va7q7uohaolu9bvl.apps.googleusercontent.com',
+    androidClientId:
+      '595812871102-sf6v5bui4fm3nquunc1dpr68sha7eb72.apps.googleusercontent.com',
+    //webClientId: '793761719542-5dfr6olefnlj5h4hur9bl1qmahv4rh50.apps.googleusercontent.com',
+    webClientId:
+      '1010390116818-e75jsc1fa7m0b4ko02e05dvasmpobbe7.apps.googleusercontent.com',
   });
 
   React.useEffect(() => {
     if (response?.type === 'success') {
       const { id_token } = response.params;
-      const credential = firebase.auth.GoogleAuthProvider.credential(id_token);
-      firebase.auth().signInWithCredential(credential)
+      const credential = GoogleAuthProvider.credential(id_token);
+      firebase
+        .auth()
+        .signInWithCredential(credential)
         .then(async (userCredential) => {
           const { user } = userCredential;
           await addUserToDatabase(user);
